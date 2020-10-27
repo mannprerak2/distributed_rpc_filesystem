abd8821f9ec8a5a6116388091ffbe43e34e58ebe
Saurabh Mittal <saurabhmittal1606@gmail.com>
2020-10-28 00:09:47 +0530

    Add encryption

---
+++ encrypt.py
@@ -0,0 +1,46 @@
+import base64
+import hashlib
+import json
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+
+# Saved keys
+from keys import FS_KEY, CLI_KEY
+
+def encrypt(raw, is_fs):
+    if is_fs:
+        key = FS_KEY
+    else:
+        key = CLI_KEY
+
+    BS = AES.block_size
+    pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+
+    raw = base64.b64encode(pad(raw).encode('utf8'))
+    iv = get_random_bytes(AES.block_size)
+
+    cipher = AES.new(key=key, mode= AES.MODE_CFB, iv= iv)
+    return base64.b64encode(iv + cipher.encrypt(raw))
+
+def decrypt(enc, is_fs):
+    if is_fs:
+        key = FS_KEY
+    else:
+        key = CLI_KEY
+        
+    unpad = lambda s: s[:-ord(s[-1:])]
+
+    enc = base64.b64decode(enc)
+    iv = enc[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CFB, iv)
+    return unpad(base64.b64decode(cipher.decrypt(enc[AES.block_size:])).decode('utf8'))
+
+if __name__ == "__main__":
+    payload = json.dumps({ 'id': "123", 'key': "123438473" })
+    print(payload)
+
+    data = encrypt(payload, is_fs=True)
+    print(data)
+
+    decrypted = decrypt(data, True)
+    print(decrypted)
---
+++ keys.py
@@ -0,0 +1,2 @@
+FS_KEY = bytes.fromhex("6184e0c8351851b7ca8d176fce40e793")
+CLI_KEY = bytes.fromhex("1089c38012d5ee2b9c8e5317b0fac1df")
